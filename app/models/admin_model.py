9aaee7f33709398680810ede00ce43bc34babd74
Wesley Rodrigo <wrodrigo33@gmail.com>
2021-07-10 14:40:35 -0300

    feat(adminModel): admin model created

---
+++ app/models/admin_model.py
@@ -0,0 +1,27 @@
+from app.config.database import db
+from sqlalchemy import Column, String, Integer
+from dataclasses import dataclass
+from werkzeug.security import generate_password_hash
+
+
+@dataclass
+class AdminModel(db.Model):
+    id: int
+    name: str
+    email: str
+    password_hash: str
+
+    __tablename__ = "admin"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(126), nullable=True)  #this one is Silvão
+    email = Column(String(126), nullable=False, unique=True)
+    password_hash = Column(String(522), nullable=False)
+
+    @property
+    def password(self):
+        raise AttributeError("Password is not acessible")
+
+    @password.setter
+    def password(self, password_to_hash):
+        self.password_hash = generate_password_hash(password_to_hash)
