1b3b446e4b9b1d0ffa31ed962dd255ade3b74fe1
Gustavo Messias <gustavo.hmessias96@gmail.com>
2021-07-10 22:25:52 -0300

    feat(entityServices): entity services created

---
+++ app/services/helper.py
@@ -0,0 +1,23 @@
+from flask_sqlalchemy.model import Model
+from flask import current_app
+
+
+def add_commit(model: Model) -> None:
+    session = current_app.db.session
+
+    session.add(model)
+    session.commit()
+
+
+def add_all_commit(list_model: list[Model]) -> None:
+    session = current_app.db.session
+
+    session.add_all(list_model)
+    session.commit()
+
+
+def delete_commit(model: Model) -> None:
+    session = current_app.db.session
+
+    session.delete(model)
+    session.commit()
