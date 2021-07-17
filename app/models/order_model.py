29c15b2db9108cbf7afb9838fbe2730820fd4da5
Wesley Rodrigo <wrodrigo33@gmail.com>
2021-07-17 17:28:23 -0300

    feat(ordersModel): created structure

---
+++ app/models/order_model.py
@@ -0,0 +1,13 @@
+from app.config.database import db
+
+from datetime import datetime
+from dataclasses import dataclass
+
+from sqlalchemy.orm import relationship, backref
+from sqlalchemy.sql.schema import ForeignKey
+from sqlalchemy import Column, String, Integer, TIMESTAMP
+
+
+@dataclass
+class AddressModel(db.Model):
+    ...
