f1d425b5d8bf7dce7f36acb8bcda3c8e3e755f8b
Wesley Rodrigo <wrodrigo33@gmail.com>
2021-07-17 18:04:02 -0300

    feat(ordersProdructModel): concluded

--- app/models/order_product_model.py
+++ app/models/order_product_model.py
@@ -1,11 +1,10 @@
 from app.config.database import db
 
-from datetime import datetime
 from dataclasses import dataclass
 
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy.sql.schema import ForeignKey
-from sqlalchemy import Column, String, Integer, TIMESTAMP
+from sqlalchemy import Column, Integer
 
 
 @dataclass
@@ -15,6 +14,14 @@ class OrderProductModel(db.Model):
     product_quantity: int
     total_price: str
 
-    __tablename__ = 'orders_products'
+    __tablename__ = "orders_products"
 
-    
+    id = Column(Integer, primary_key=True)
+    sold_price = Column(Integer, nullable=False)
+    product_quantity = Column(Integer, nullable=False)
+    total_price = Column(Integer, nullable=False)
+    order_id = Column(Integer, ForeignKey("orders.id"))
+    product_id = Column(Integer, ForeignKey("products.id"))
+
+    orders = relationship("OrderModel", backref=backref("orders_products"))
+    products = relationship("ProductModel", backref=backref("orders_products"))
