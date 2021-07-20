00bd6096de10d4b1a5f13c26ce941c30b773b95d
Gustavo Messias <gustavo.hmessias96@gmail.com>
2021-07-20 15:29:47 -0300

    feat(cartService): add is_empty en total on response /cart and refact cartView

--- app/views/cart_view.py
+++ app/views/cart_view.py
@@ -12,33 +12,14 @@ from app.exc import NotFoundEntityError
 from app.services.helper import message_integrety_error
 from datetime import datetime
 from app.services.auth_service import admin_required, customer_required
+from app.services.cart_service import CartServices
 
 
 class CartResource(Resource):
     @customer_required()
     def get(self, customer_id):
         try:
-            current_customer = EntityServices.get_entity_by_id(
-                CustomerModel, customer_id
-            )
-            customer_cart: CartModel = current_customer.cart
-
-            list_products = EntityServices.get_all_entity_by_keys(
-                CartProductModel, cart_id=customer_cart.id
-            )
-            total_price_cart = sum(product.total_price for product in list_products)
-
-            if list_products:
-                is_empty = False
-
-                updated_data = dict(total_price=total_price_cart, is_empty=is_empty)
-                updated_cart = EntityServices.update_entity(customer_cart, updated_data)
-
-            cart_dict = {
-                "products": list_products,
-                "total_price": updated_cart.total_price,
-                "is_empty": updated_cart.is_empty,
-            }
+            cart_dict = CartServices.get_cart(customer_id)
             return make_response(jsonify(cart_dict), HTTPStatus.OK)
 
         except NotFoundEntityError as error:
@@ -52,20 +33,8 @@ class CartProductResource(Resource):
         quantity_product = int(request.args.get("quantity_product", 1))
 
         try:
-            current_customer = EntityServices.get_entity_by_id(
-                CustomerModel, customer_id
-            )
-            current_product = EntityServices.get_entity_by_id(ProductModel, product_id)
-            total_price = current_product.current_price * quantity_product
-            customer_cart: CartModel = current_customer.cart
-            cart_product: dict = {
-                "cart_id": customer_cart.id,
-                "product_id": product_id,
-                "quantity_product": quantity_product,
-                "total_price": total_price,
-            }
-            created_cart_product = EntityServices.create_entity(
-                CartProductModel, cart_product
+            created_cart_product = CartServices.add_product_to_cart(
+                quantity_product, customer_id, product_id
             )
             return make_response(jsonify(created_cart_product), HTTPStatus.CREATED)
 
@@ -84,22 +53,8 @@ class CartProductResource(Resource):
         quantity_product = int(request.args.get("quantity_product", 1))
 
         try:
-            current_customer = EntityServices.get_entity_by_id(
-                CustomerModel, customer_id
-            )
-            current_product = EntityServices.get_entity_by_id(ProductModel, product_id)
-            updated_price = current_product.current_price * quantity_product
-            customer_cart: CartModel = current_customer.cart
-            data_cart_product: dict = {
-                "quantity_product": quantity_product,
-                "total_price": updated_price,
-                "updated_at": datetime.now(),
-            }
-            current_cart_product = EntityServices.get_entity_by_keys(
-                CartProductModel, cart_id=customer_cart.id, product_id=product_id
-            )
-            updated_cart_product = EntityServices.update_entity(
-                current_cart_product, data_cart_product
+            updated_cart_product = CartServices.update_cart_product(
+                quantity_product, customer_id, product_id
             )
             return make_response(jsonify(updated_cart_product), HTTPStatus.OK)
 
@@ -109,14 +64,7 @@ class CartProductResource(Resource):
     @customer_required()
     def delete(self, customer_id, product_id):
         try:
-            current_customer = EntityServices.get_entity_by_id(
-                CustomerModel, customer_id
-            )
-            customer_cart: CartModel = current_customer.cart
-            current_cart_product = EntityServices.get_entity_by_keys(
-                CartProductModel, cart_id=customer_cart.id, product_id=product_id
-            )
-            EntityServices.delete_entity(current_cart_product)
+            CartServices.delete_cart_product(customer_id, product_id)
             return "", HTTPStatus.NO_CONTENT
 
         except NotFoundEntityError as error:
